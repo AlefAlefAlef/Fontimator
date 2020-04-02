7f9b2c676fd39f19bb23f5a008e393639bc6c9ea
Avraham Cornfeld <avraham.cornfeld@gmail.com>
2020-04-02 11:21:09 +0300

    removed line (git test)

--- includes/class-fontimator.php
+++ includes/class-fontimator.php
@@ -337,7 +337,6 @@ class Fontimator {
 	 * @access   private
 	 */
 	private function define_public_hooks() {
-
 		$plugin_public = new Fontimator_Public( $this->get_plugin_name(), $this->get_version() );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
